91d5680a4ce0330837cdfc3ac0534a92a6809747
vaishnavi.p <vaishuu@gmail.com>
2025-02-05 09:55:51 +0530

    changes done in add.java file

--- add.java
+++ add.java
@@ -1,5 +1,8 @@
 public class add{
     public static void main(String args[]){
       System.out.println(".()");
+      System.out.println("hh");
+      System.out.println("hhhjk");
+      System.out.println("hhser");
     }
 }
--- sub.java
+++ sub.java
@@ -2,5 +2,7 @@ public class sub{
     public static void main(String[] args) {
         System.out.println(".()");
         System.out.println(".()");
+    System.out.println("hiiiiiiiiiii");
+
     }
 }
